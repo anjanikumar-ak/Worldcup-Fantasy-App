f02472d753ddedb1a3dda96e2c8ba9da14a22230
anjanikumar-ak <81159314+anjanikumar-ak@users.noreply.github.com>
2021-10-23 21:57:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>Manit Gang IPL Fantasy Teams</title>
+        <title>WOLRD T20 FANTASY</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
